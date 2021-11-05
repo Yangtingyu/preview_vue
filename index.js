8cc6cfbf265c3705b576dfd6742fd0a45137b3cc
whyusb@gmail.com <whyusb@gmail.com>
2021-11-05 15:50:08 +0800

    process.pwd

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const history = require('connect-history-api-fallback');
 const app = express();
 const open = require('open');
 const path = require('path');
+const process = require('process');
 
 
-const staticFileMiddleware = express.static(path.join(__dirname + '/dist'));
+
+const staticFileMiddleware = express.static(path.join(process.cwd() + '/dist'));
 app.use(staticFileMiddleware);
 app.use(history({
   disableDotRule: true,
@@ -14,7 +16,7 @@ app.use(history({
 app.use(staticFileMiddleware);
 
 app.get('/', function (req, res) {
-  res.render(path.join(__dirname + '/dist/index.html'));
+  res.render(path.join(process.cwd() + '/dist/index.html'));
 });
 
 const port = 5555;
