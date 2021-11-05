ef1f02b8738c02e7269a72ac0e4afc5c92bac585
whyusb@gmail.com <whyusb@gmail.com>
2021-11-05 10:47:12 +0800

    init

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const history = require('connect-history-api-fallback');
+const app = express();
+const open = require('open');
+app.use('/', express.static(path.join(__dirname, 'dist')));
+
+
+const staticFileMiddleware = express.static('dist');
+app.use(staticFileMiddleware);
+app.use(history({
+  disableDotRule: true,
+  verbose: true
+}));
+app.use(staticFileMiddleware);
+
+const port = 5555;
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
+
+open('http://localhost:5555');
